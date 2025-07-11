9d68dc9a899dce7dcc47a5e63ab24325f6a9a82e
Taylor Plewe <tplewe@outlook.com>
2025-07-10 21:09:33 -0600

    parse game titles and notes

--- src/main.zig
+++ src/main.zig
@@ -20,12 +20,12 @@ const std = @import("std");
 const Review = struct {
     title: []const u8,
     score: u8,
-    body: []const u8,
+    body: []u8,
 };
 const Note = []const u8;
 const Element = union(enum) {
-    Review,
-    Note,
+    Review: Review,
+    Note: Note,
 };
 
 pub fn main() !void {
@@ -46,8 +46,23 @@ pub fn main() !void {
         std.process.exit(1);
     };
 
-    var blocks: std.MultiArrayList(Review) = .{};
+    var blocks = std.ArrayList(Element).init(arena.allocator());
+    var currReview: ?Review = null;
     const file_reader = file.reader();
+
+    // iterate over each line
+    while (try file_reader.readUntilDelimiterOrEofAlloc(arena.allocator(), '\n', 65536)) |line| {
+        if (line.len > 2 and std.mem.eql(u8, line[0..2], "--")) {
+            const note: Note = try arena.allocator().dupe(u8, line);
+            try blocks.append(Element{ .Note = note });
+        } else if (line.len > 4 and std.mem.eql(u8, line[0..4], "### ")) {
+            currReview = .{
+                .title = try arena.allocator().dupe(u8, line[4..]),
+                .score = 0,
+                .body = "",
+            };
+        }
+    }
 
     try stdOutWriter.print("\x1b[32mOK:\x1b[0m successfully wrote to game review markdown file.\n", .{});
 }
